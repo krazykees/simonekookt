0c0059f6ed97a3431d4a08865226224b699fb7a5
krazykees <krazykees@user.noreply.github.com>
2014-04-19 14:54:41 +0200

    added dj_static gunicorn to Procfile settings.py edit

--- simonekookt/settings.py
+++ simonekookt/settings.py
@@ -98,10 +98,10 @@ import dj_database_url
 if os.getcwd() == "/app":
     DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
-if os.getcwd() == '/app':
-    STATIC_ROOT = 'staticfiles'
-    STATIC_URL = '/static/'
 
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
