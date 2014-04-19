b9df1ba5571d6da5ca150063530632da055abfd6
krazykees <krazykees@user.noreply.github.com>
2014-04-19 14:35:23 +0200

    added dj_static gunicorn to Procfile

--- simonekookt/wsgi.py
+++ simonekookt/wsgi.py
@@ -11,7 +11,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simonekookt.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+# application = get_wsgi_application()
 
 from dj_static import Cling
 
